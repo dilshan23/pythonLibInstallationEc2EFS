0e14a89faea73ca776b41264bd205f54f6522fae
dilshan23 <38337550+dilshan23@users.noreply.github.com>
2020-07-03 10:45:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ cd /home/ec2-user
 mkdir lib
 
 # Setup the credentials for the AWS CLI
-aws configure set aws_access_key_id $1
-aws configure set aws_secret_access_key $2
+#aws configure set aws_access_key_id $1
+#aws configure set aws_secret_access_key $2
 
 # ++++++++++++ END  +++++++++++++
